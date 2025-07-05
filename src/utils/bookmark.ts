26a21e02c179222d8f971ee66378b49afbbb3ad7
OWL-WIN <owl>
2025-07-05 11:50:40 +0800

    fetch web head

--- src/utils/bookmark.ts
+++ src/utils/bookmark.ts
@@ -313,15 +313,20 @@ interface GroupedData {
 }
 
 // Scrape website metadata
-const scrapeWebsite = async (url: string): Promise<ScrapedData> => {
+export const scrapeWebsite = async (url: string): Promise<ScrapedData> => {
   const CORS_PROXY = "https://corsproxy.io/?";
   try {
     const response = await fetch(CORS_PROXY + encodeURIComponent(url), {
+      // const response = await fetch(url, {
       headers: {
         "User-Agent":
           "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/128.0.0.0 Safari/537.36",
         Accept:
           "text/html,application/xhtml+xml,application/xml;q=0.9,image/avif,image/webp,*/*;q=0.8",
+
+        // "Access-Control-Allow-Origin": "http://localhost:3000",
+        // "Access-Control-Allow-Methods": "*",
+        // "Access-Control-Allow-Headers": "*",
       },
     });
     if (!response.ok) throw new Error(`HTTP error: ${response.status}`);
