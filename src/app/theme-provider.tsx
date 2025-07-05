ac482778f99727d57dff4e370d272c7eeb33d0c4
OWL-WIN <owl>
2025-07-05 17:13:21 +0800

    fix export

--- src/app/theme-provider.tsx
+++ src/app/theme-provider.tsx
@@ -13,7 +13,7 @@ export function ThemeProvider({ children }: { children: React.ReactNode }) {
   return (
     <NextThemesProvider
       attribute="class"
-      defaultTheme={"light"}
+      defaultTheme={"dark"}
       enableSystem
       disableTransitionOnChange
     >
